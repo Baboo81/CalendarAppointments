014f61653568d4d3bda1eb3a78bd72cb45a8a9b3
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-08-21 16:27:36 +0200

    Create function reducer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,18 @@
 "use strict";
 
+let Case = document.getElementsByClassName('case');
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+
+const monthName = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+
+const UP_MONTH ='upMonth';
+const DOWN_MONTH = 'downMonth';
+
+function CALENDRIER_REDUCER(action) {
+
+}
+
