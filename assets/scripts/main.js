6d5e579af29bde28faed33b996f5ca5ad8e330e1
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-08-30 10:24:09 +0200

    main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Calendrier :
 let Cases = document.getElementsByClassName('case');
 
 let date = new Date();
@@ -87,3 +88,22 @@ function getFevrierLength(year) {
         else return 28
     }
  
+//Déplacement des blocs :
+
+const bloc = document.getElementById('subContainer');
+let xPos = 0;
+let yPos = 0;
+let angle = 0;
+
+function animate () {
+    //xPos += "300px";
+    //xPos -= "300px";
+    //angle += .5; 
+    //yPos = Math.round(Math.sin(angle));
+
+    bloc.style.transform = `translate3d(${xPos}, ${yPos}, 0)`;
+
+    requestAnimationFrame(animate);
+};
+
+animate();
