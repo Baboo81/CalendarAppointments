e99353b2d3fbe6e2603005760730fbbd17439bab
Baboo81 <magicbaboo@gmail.com>
2023-10-26 19:35:32 +0200

    main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,14 +14,22 @@ function animateDandelion () {
 const togg = document.getElementById('togg');
 const togg2 = document.getElementById('togg2');
 const content = document.getElementById('cardContent');
+const content2 = document.getElementById('cardContent2');
 
 
 togg.addEventListener("click", () => {
-    if(getComputedStyle(content, togg2).display != "none") {
+    if(getComputedStyle(content, content2).display != "none") {
         content.style.display = "none";
     } else {
         content.style.display = "block";
-        togg2.style.display = "block";
+    }
+});
+
+togg2.addEventListener("click", () => {
+    if(getComputedStyle(content, content2).display != "none") {
+        content2.style.display = "none";
+    } else {
+        content2.style.display = "block";
     }
 });
 
@@ -29,6 +37,7 @@ togg.addEventListener("click", () => {
 function collapsed () {
     if(getComputedStyle(content).display != "none") {
         content.style.display = "none";
+        content2.style.display = "none";
   }
 };
 
