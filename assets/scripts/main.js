b8bd0c82f500f13b397977a2b148ebff52ad13f7
Baboo81 <magicbaboo@gmail.com>
2023-11-12 10:55:51 +0100

    JS animation ball

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,19 +4,24 @@
 const ball = document.getElementById('ball1');
 let leftPos = 0;
 let dir = -1;
+let current_rotation = 0; 
 
 ball.style.position = "absolute";
 
 function ballAnimate () {
-    if (leftPos == 300) {
+
+    if (leftPos == 900) {
         dir = 1
     } else if 
-        (leftPos == -50) {
+        (leftPos == -100) {
         dir = -1
     }
     
     leftPos += -2 * dir;
+    current_rotation += 1;
+
     ball.style.left = `${leftPos}px`;
+    ball.style.transform = 'rotate('  + current_rotation + 'deg)';
 
     requestAnimationFrame(ballAnimate);
 };
